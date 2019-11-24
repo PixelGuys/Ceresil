f175ee1057ad20d003107391aa6c643a4590d29c
ZaUserA <moddedminecrafttutorials31@gmail.com>
2019-11-24 18:23:48 +0200

    Basic Player Movement

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+	public float MouseSensitivity = 100F;
+	public Transform PlayerTransform;
+	
+	private float XRotation;
+    private float MouseX;
+	private float MouseY;
+
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+    void Update()
+    {
+        MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+		MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+		XRotation -= MouseY;
+		XRotation = Mathf.Clamp(XRotation, -90F, 90F);
+		transform.localRotation = Quaternion.Euler(XRotation, 0F, 0F);
+		PlayerTransform.Rotate(Vector3.up * MouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public CharacterController controller;
+	public float movementSpeed = 12F;
+	
+	private Vector3 Movement;
+	private float XAxis;
+	private float ZAxis;
+
+    void Update()
+    {
+        XAxis = Input.GetAxis("Horizontal");
+		ZAxis = Input.GetAxis("Vertical");
+		Movement = transform.right * XAxis + transform.forward * ZAxis;
+		controller.Move(Movement * movementSpeed * Time.deltaTime);
+    }
+}
