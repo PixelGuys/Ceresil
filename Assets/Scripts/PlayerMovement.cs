bdaa9eb7d2c444b5ab575545444ed60e32034873
zenith391 <pawalamde@gmail.com>
2019-11-24 20:24:36 +0100

    Some more player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
 
 	void ApplyGravity(Attractor attractor) {
         Vector3 dir = attractor.gameObject.transform.position - gameObject.transform.position;
-        float magSqr = dir.sqrMagnitude;
+        float magSqr = dir.sqrMagnitude / 10;
         if (magSqr > 0.0001f) {
             body.AddForce(attractor.gravityForce * dir.normalized / magSqr,
                 ForceMode.Acceleration);
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
 	void Start()
     {
         body = gameObject.GetComponent<Rigidbody>();
-        body.useGravity = false;
+        body.useGravity = false; // IMPORTANT! Unity's gravity isn't the gravity we need!
     }
 
     void Update()
@@ -32,7 +32,8 @@ public class PlayerMovement : MonoBehaviour
         xAxis = Input.GetAxis("Horizontal");
 		zAxis = Input.GetAxis("Vertical");
 		Movement = transform.right * xAxis + transform.forward * zAxis;
-		body.AddForce(Movement * movementSpeed);
+		//body.AddForce(Movement * movementSpeed);
+        transform.position += (Movement * movementSpeed); // temporary in search of better solution
     }
 
 	void FixedUpdate()
